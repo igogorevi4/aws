bd60de637f9cca6ea95105dff0a9acdc353c3d74
Yaroslav Pozdnyakov <yaroslav.pozdnyakov@gmail.com>
2019-04-02 12:54:50 +0300

    added deactivating keys that are never used

--- rotation.unused.access.keys.py
+++ rotation.unused.access.keys.py
@@ -2,6 +2,7 @@
 
 # This script revokes users' key if there is no any activity during last 30 days. Or if key had been created more that 30 days ago and never used.
 # as AWS recommends https://docs.aws.amazon.com/IAM/latest/UserGuide/id_credentials_access-keys.html#rotating_access_keys_cli
+# Run in AWS Lambda by schedule (CloudWatch Event) https://docs.aws.amazon.com/AmazonCloudWatch/latest/events/RunLambdaSchedule.html 
 
 import boto3
 import datetime
@@ -9,38 +10,34 @@ import datetime
 resource = boto3.resource('iam')
 client = boto3.client('iam')
 today = datetime.date.today()
-# Required inactivity threshold
-threshold = datetime.timedelta(days=2)   # !!!!! Change to required thresholds
-KEY = ''        # !!!!
+# Required inactivity threshold. Change to required thresholds
+threshold = datetime.timedelta(days=1)
+# some trick to get keys that are never used
+notAvaiableKey = 'N/A'
 
 for user in resource.users.all():
-    print (user.user_name)      # debug
     Metadata = client.list_access_keys(UserName=user.user_name)
     if Metadata['AccessKeyMetadata']:
-        # print(Metadata)     # debug
         for key in user.access_keys.all():
-            # print (key.access_key_id)       # debug
             AccessId = key.access_key_id
             Status = key.status
             LastUsed = client.get_access_key_last_used(AccessKeyId=AccessId)
-            # working with only active keys
+            # notice only active keys
             if (Status == "Active"):
-                print ('Key is active:', AccessId)  # debug
-                print(LastUsed['AccessKeyLastUsed'])        # debug
-                # if KEY not in LastUsed['AccessKeyLastUsed']:
-                #     print('Key is never used')
-                #     creationDate = Metadata['AccessKeyMetadata'][0]['CreateDate'].date()
-                #     print('Creation date:',creationDate)
-                # else:
-                lastUsedTimestamp = LastUsed['AccessKeyLastUsed'].get('LastUsedDate')
-                last = lastUsedTimestamp.date()
-                # # Check diff between lastUsedDate and today
-                # inactiveDays = today - last
-                # # print(inactiveDays.days)      # debug
-                # if (inactiveDays > threshold):
-                #     # print('Inactive days:', inactiveDays.days)      # debug
-                #     deactivate = client.update_access_key(AccessKeyId=AccessId, Status='Inactive', UserName=user.user_name)
-                #     # print(AccessId, ' key has been deactivated')    # debug
+                # check if key is never used
+                if (notAvaiableKey in LastUsed['AccessKeyLastUsed']['ServiceName']):
+                    creationDate = Metadata['AccessKeyMetadata'][0]['CreateDate'].date()
+                    unsedDays = today - creationDate
+                    if (unsedDays >= threshold):
+                        # Deactivating key
+                        client.update_access_key(AccessKeyId=AccessId, Status='Inactive', UserName=user.user_name)
+                # else check its last used date
+                else:
+                    lastUsedTimestamp = LastUsed['AccessKeyLastUsed'].get('LastUsedDate')
+                    last = lastUsedTimestamp.date()
+                    inactiveDays = today - last
+                    if (inactiveDays >= threshold):
+                        # Deactivating key
+                        deactivating = client.update_access_key(AccessKeyId=AccessId, Status='Inactive', UserName=user.user_name)
             else:
-                print ('Key is inactive:', AccessId) 
-                print(LastUsed['AccessKeyLastUsed']) 
+                print('Key is inactive:', AccessId)
