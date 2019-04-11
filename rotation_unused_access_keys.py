192d944dcdd07b1fac61dca4a617e54a0d8fb18e
Yaroslav Pozdnyakov <yaroslav.pozdnyakov@gmail.com>
2019-04-11 15:18:52 +0300

    added logging; added email notification before expiration

--- rotation_unused_access_keys.py
+++ rotation_unused_access_keys.py
@@ -2,26 +2,21 @@
 
 import boto3
 import datetime
-# import sys
 import os
 
 resource = boto3.resource('iam')
 client = boto3.client('iam')
 today = datetime.date.today()
 # Reading required inactivity threshold
-# threshold_days = int(sys.stdin.read())
-# Environment variables
-email_sender = str(os.environ['EMAILSENDER'])
-email_recipient = str(os.environ['EMAILRECIPIENT'])
 threshold_days = int(os.environ['THRESHOLD'])
 threshold = datetime.timedelta(days=threshold_days)
-# some trick to get keys that are never useds
-not_available_key = 'N/A'
+email_sender = "eamilv@example.com"    # replace it to admin email
+admin_email = "admin@example.com"
 
 
-def send_message(user):
+def send_message(email_subject, email_body, email_recipient):
     ses = boto3.client('ses')
-    email = ses.send_email(
+    message = ses.send_email(
         Source=email_sender,
         Destination={
             'ToAddresses': [
@@ -30,78 +25,86 @@ def send_message(user):
         },
         Message={
             'Subject': {
-                'Data': 'AWS Access Key deactivated',
+                'Data': email_subject,
                 'Charset': 'UTF-8',
             },
             'Body': {
                 'Text': {
                     'Charset': 'UTF-8',
-                    'Data': 'Access key for user ' + user.user_name + ' has been deactivated.'
+                    'Data': email_body,
                 },
             },
         },
     )
+    print(str(datetime.datetime.now()) + ": Notification has been successfully sent")
+
+
+# notification to user berore AccessKey expirartion
+def create_preliminary_email_content(threshold, existence_days, user, email_recipient):
+    days_to_expiration = (threshold - existence_days).days
+    if days_to_expiration == 10 or days_to_expiration == 5:
+        email_subject = "AWS AccessKey Expiration"
+        email_body = "Your access key will expire in " + str(days_to_expiration) + " days"
+        notify_before_expiration(user, email_recipient, email_subject, email_body)
+        print(str(datetime.datetime.now()) + ": User " + user.name +
+              " has just been notified that " + str(days_to_expiration) +
+              " days left to AccessKey expiration")
+
+
+def notify_before_expiration(user, email_recipient, email_subject, email_body):
+    send_message(email_subject, email_body, email_recipient)
+    print(str(datetime.datetime.now()) +
+          ": User " + user.name +
+          " has just been notified that 10 days left to AccessKey expiration")
+
+
+def get_user_email(user):
+    try:
+        user_tags = client.list_user_tags(
+            UserName=user.name,
+        )
+        for tags in user_tags['Tags']:
+            if tags['Key'] == 'email':
+                email = tags['Value']
+                return email
+    except:
+        error_message = "There is no any email address associated with user " + user.name
+        email_subject = "AWS: need to add tag to user"
+        email_body = error_message
+        email_recipient = admin_email
+        send_message(email_subject, email_body, email_recipient)
+        print(str(datetime.datetime.now()) + ": " + error_message)
 
 
 def deactivate_access_key(access_id, user):
     client.update_access_key(AccessKeyId=access_id, Status='Inactive',
-                             UserName=user.user_name)
-
-
-def delete_obsolete_key(access_id, user):
-    client.delete_access_key(AccessKeyId=access_id,
-                             UserName=user.user_name)
-
-
-def create_new_access_key(user):
-    client.create_access_key(UserName=user.user_name)
+                             UserName=user.name)
 
 
 def main_logic():
     for user in resource.users.all():
-        if user.name == 'test_user1' or user.name == 'kklk045':
-            clean_key_for_user(user)
+        deactivate_key_for_user(user)
 
 
-def clean_key_for_user(user):
-    metadata = client.list_access_keys(UserName=user.user_name)
-    if not metadata['AccessKeyMetadata']:
-        return
-
+def deactivate_key_for_user(user):
     for key in user.access_keys.all():
-        clean_key(user, key, metadata)
+        access_id = key.access_key_id
+        key_status = key.status
+        creation_date = key.create_date.date()
+        if key_status == "Active":
+            expiration_key(access_id, user, creation_date)
+            
 
-
-def clean_key(user, key, meta):
-    access_id = key.access_key_id
-    key_status = key.status
-    last_used = client.get_access_key_last_used(AccessKeyId=access_id)
-    if key_status != "Active":
-        return
-
-    # check if key is never used
-    if not_available_key in last_used['AccessKeyLastUsed']['ServiceName']:
-        deactivate_never_used_key(meta, access_id, user)
-    else:
-        deactivate_too_old_key(last_used, access_id, user)
-
-
-def deactivate_never_used_key(meta, access_id, user):
-    access_key_metadata = meta['AccessKeyMetadata'][0]
-    creation_date = access_key_metadata['CreateDate'].date()
-    unused_days = today - creation_date
-    if unused_days > threshold:
+def expiration_key(access_id, user, creation_date):    
+    existence_days = today - creation_date
+    email_recipient = get_user_email(user)
+    create_preliminary_email_content(threshold, existence_days, user, email_recipient)
+    if existence_days > threshold:
         deactivate_access_key(access_id, user)
-        send_message(user)
-
-
-def deactivate_too_old_key(last_used, access_id, user):
-    last_used_timestamp = last_used['AccessKeyLastUsed'].get('LastUsedDate')
-    last = last_used_timestamp.date()
-    uinactive_days = today - last
-    if uinactive_days > threshold:
-        deactivate_access_key(access_id, user)
-        send_message(user)
-
+        email_subject = "AWS Access Key deactivated"
+        email_body = "Your access key has been deactivated."
+        send_message(email_subject, email_body, email_recipient)
+        print(str(datetime.datetime.now()) + ": Access key for user " + user.name + " has been deactivated")
+        
 
 main_logic()
